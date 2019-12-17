b44928868ccfe66ac63a05b4b391f3594bed0f1a
Sandro Maisuradze <sandromaisuradze32@gmail.com>
2019-12-17 15:29:50 +0400

    added messenger function

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const socket = io('http://localhost:3000')
-const messageForm = 
-document.getElementById('send-container')
 
-const messageInput = 
-document.getElementById('message-input')
+const messageContainer = document.getElementById('send-container');
+
+const messageForm = document.getElementById('send-container');
+
+const messageInput = document.getElementById('message-input');
 
 socket.on('chat-message', data =>{
-    console.log(data)
-})
+    appendMessage(data);
+});
 
 messageForm.addEventListener('submit', e => {
-    preventDefault();
+    e.preventDefault();
     const message = messageInput.value;
     socket.emit('send-chat-message', message);
     messageInput.value = '';
 
 });
+
+function appendMessage(message){
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+};
