b408b712dddb38b926ca0fa069d04fc5d796f9dc
Sandro Maisuradze <sandromaisuradze32@gmail.com>
2019-12-17 16:06:08 +0400

    added other user joininig message (not working Yet)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* 
+Declaring variables/constants 
+*/
 const socket = io('http://localhost:3000')
 
 const messageContainer = document.getElementById('send-container');
@@ -6,10 +9,23 @@ const messageForm = document.getElementById('send-container');
 
 const messageInput = document.getElementById('message-input');
 
+const name = prompt('What is Your Name?');
+
+appendMessage('you Joined');
+socket.emit('new-User', name);
+
+//appending chat message
 socket.on('chat-message', data =>{
     appendMessage(data);
 });
-
+//appending joined user name
+socket.on('user-connected', name =>{
+    appendMessage (`${name} connected`);
+});
+/*
+preventing page from being refreshed and updating
+input field after message was submitted
+*/
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
@@ -18,6 +34,7 @@ messageForm.addEventListener('submit', e => {
 
 });
 
+/* Displaying recieved message*/
 function appendMessage(message){
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
